5f2ff8eba06d646f06a99a367a2bdd3ea5f6e603
alv67 <alv67.web@gmail.com>
2021-04-25 22:19:44 +0200

    Lezione 4

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const app = Vue.createApp({
   data() {
     return {
-      product: 'Socks'
+      product: 'Calzini',
+      description: 'Un fantastico paio di calzini comodi e colorati.',
+      image: './assets/images/socks_green.jpg',
+      url: 'http://www.google.com/',
+      inventory: 8,
+      onSale: true
     }
   }
 })
